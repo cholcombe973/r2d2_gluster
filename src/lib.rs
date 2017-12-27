ed8881acd4e8a2c3855e7c9321f08e64ff4d9fb4
Chris Holcombe <xfactor973@gmail.com>
2017-12-27 14:29:43 -0800

    Add logging and rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
 extern crate gfapi_sys;
+#[macro_use]
+extern crate log;
 extern crate r2d2;
 
 use gfapi_sys::gluster::*;
 use r2d2::ManageConnection;
 
-pub struct GlusterPool{
+pub struct GlusterPool {
     server: String,
     port: u16,
     volume: String,
 }
 
-impl GlusterPool{
-    pub fn new(server: &str, port: u16, volume: &str) -> Self{
-        GlusterPool{
+impl GlusterPool {
+    pub fn new(server: &str, port: u16, volume: &str) -> Self {
+        GlusterPool {
             server: server.to_owned(),
             port: port,
             volume: volume.to_owned(),
@@ -24,16 +26,22 @@ impl ManageConnection for GlusterPool {
     type Connection = Gluster;
     type Error = GlusterError;
 
-    fn connect(&self) -> Result<Self::Connection, Self::Error>{
+    fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        debug!(
+            "Connecting to gluster: {}:{}/{}",
+            self.server, self.port, self.volume
+        );
         let conn = Gluster::connect(&self.volume, &self.server, self.port)?;
         Ok(conn)
     }
 
-    fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error>{
-       Ok(()) 
+    fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
+        trace!("is_valid");
+        Ok(())
     }
 
-    fn has_broken(&self, conn: &mut Self::Connection) -> bool{
-       false 
+    fn has_broken(&self, conn: &mut Self::Connection) -> bool {
+        trace!("has_broken");
+        false
     }
 }
